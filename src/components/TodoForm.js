677c13d0cb0b5703ed2fa3fb87e56e20ff731fd1
cam1la444 <camila24melara@gmail.com>
2024-09-09 23:32:22 -0600

    fixed addTodo function

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 
-export const TodoForm = (addTodo) =>{
+export const TodoForm = ({addTodo}) =>{
     const [value, setValue] = useState('')
 
     const handleSubmit = e =>{
