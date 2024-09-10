a74514001c63029c0117152825eddd2e826d414e
cam1la444 <camila24melara@gmail.com>
2024-09-09 23:26:15 -0600

    TodoForm y wrapper

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export const TodoForm = () =>{
+export const TodoForm = (addTodo) =>{
+    const [value, setValue] = useState('')
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+
+        addTodo(value)
+
+        setValue('')
+    }
+
     return(
-        <div>TodoForm</div>
+        <form className='TodoForm' onSubmit={handleSubmit}>
+            <input type='text' className='todo-input' value={value} placeholder='¿Cuales son las tareas del dia?'
+            onChange={(e)=> setValue(e.target.value)}/>
+            <button type='submit' className='todo-btn'>Agregue la tarea</button>
+        </form>
     )
 }
